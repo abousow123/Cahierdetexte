dc58afbb703958ea9241de73921009a32a90b477
Your Name <you@example.com>
2017-08-02 04:59:14 +0000

    ajouter classe Expandable

--- app/src/main/java/com/example/sow_a/cahierdetexte/Accueil_Fragment.java
+++ app/src/main/java/com/example/sow_a/cahierdetexte/Accueil_Fragment.java
@@ -23,8 +23,11 @@ public class Accueil_Fragment extends Fragment {
 
     ArrayList<Matiere> matiereArrayList ;
 
+    ArrayList<String> ab;
+    ArrayList<String> ab1;
+
     DAO dao ;
-    ListView listView ;
+    ListView listViewtab1,listViewtab2,listViewtab2_suite ;
 
 
     public Accueil_Fragment() {
@@ -41,6 +44,18 @@ public class Accueil_Fragment extends Fragment {
         matiereArrayList = dao.allMatiere(getContext()) ;
         //  listView = (ListView)getView()findViewById(R.id.listMatiere); ;
 
+        ab = new ArrayList<String>();
+        ab.add("acd");
+        ab.add("gbv");
+        ab.add("bnn");
+        ab.add("bnbn");
+
+        ab1 = new ArrayList<String>();
+        ab1.add("gfbnhvbvn");
+        ab1.add("vghbvnbn");
+        ab1.add("nv,bjgjh");
+        ab1.add("bvhghhvvh");
+
     }
 
 
@@ -51,10 +66,19 @@ public class Accueil_Fragment extends Fragment {
         View rootView =  inflater.inflate(R.layout.fragment_accueil_, container, false);
 
         ArrayAdapter<Matiere> adapter = new MyListAdapter() ;
+        ArrayAdapter<String> adapter1 = new MyListAdapter1() ;
+        ArrayAdapter<String> adapter2 = new MyListAdapter2() ;
 
-        listView = (ListView)rootView.findViewById(R.id.listMatiere);
+        listViewtab1 = (ListView)rootView.findViewById(R.id.listMatiere);
+        listViewtab2 = (ListView)rootView.findViewById(R.id.list_cour);
+        listViewtab2_suite = (ListView)rootView.findViewById(R.id.list_cour2) ;
 
-        listView.setAdapter(adapter);
+
+
+        listViewtab1.setAdapter(adapter);
+
+        listViewtab2.setAdapter(adapter1);
+//        listViewtab2_suite.setAdapter(adapter2);
 
         TabHost host = (TabHost)rootView.findViewById(R.id.tabHost);
         host.setup();
@@ -115,6 +139,83 @@ public class Accueil_Fragment extends Fragment {
             return (row);
         }
     }
+
+
+    class MyListAdapter1 extends ArrayAdapter<String> {
+
+
+
+        public   MyListAdapter1() {
+
+            super(Accueil_Fragment.super.getContext(),R.layout.listview_cour,ab);
+
+        }
+
+        public void onClick(View view) {
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater  lis = getActivity().getLayoutInflater() ;
+            View row = lis.inflate(R.layout.listview_cour, parent, false);
+            TextView label = (TextView) row.findViewById(R.id.textcour1);
+            //triTabChaine(sg);
+            label.setText(ab.get(position));
+
+         /*  ImageView icon = (ImageView) row.findViewById(R.id.icon);
+
+            if (etudiants.get(position).getSexe()
+                    .equalsIgnoreCase("Femme"))
+                icon.setImageResource(R.drawable.f1);
+            if (etudiants.get(position).getSexe()
+                    .equalsIgnoreCase("Homme"))
+                icon.setImageResource(R.drawable.h1);*/
+
+            return (row);
+        }
+    }
+
+
+    class MyListAdapter2 extends ArrayAdapter<String> {
+
+
+
+        public   MyListAdapter2() {
+
+            super(Accueil_Fragment.super.getContext(),R.layout.listview_cour_suite,ab1);
+
+        }
+
+        public void onClick(View view) {
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater  lis = getActivity().getLayoutInflater() ;
+            View row = lis.inflate(R.layout.listview_cour_suite, parent, false);
+            TextView label = (TextView) row.findViewById(R.id.te1);
+            //triTabChaine(sg);
+            label.setText(ab1.get(position) );
+
+         /*  ImageView icon = (ImageView) row.findViewById(R.id.icon);
+
+            if (etudiants.get(position).getSexe()
+                    .equalsIgnoreCase("Femme"))
+                icon.setImageResource(R.drawable.f1);
+            if (etudiants.get(position).getSexe()
+                    .equalsIgnoreCase("Homme"))
+                icon.setImageResource(R.drawable.h1);*/
+
+            return (row);
+        }
+    }
+
+
+
+
+
 
 
 }
